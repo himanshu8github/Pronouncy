fcbc44a392bd25645af5e0d768f04eecc2b1c367
Himanshu <himanshukakran8@gmail.com>
2025-07-19 17:01:38 +0530

    add testing route

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -4,9 +4,14 @@ import { registerUser, loginUser } from '../controllers/auth.controller.js';
 const router = express.Router();
 
 
+router.get('/test', (req, res) => {
+  res.json({ message: "Auth route working!" });
+});
+
+// Signup Route
 router.post('/signup', registerUser);
 
-
+// Login Route
 router.post('/login', loginUser);
 
 export default router;
